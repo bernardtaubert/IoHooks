f594adb24eee6c54899f1122924a2882641d0ff3
bernardtaubert <taubert.bernard@googlemail.com>
2024-02-04 12:09:07 +0100

    Initial commit

---
+++ OutputEmulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+class Program
+{
+    private const int INPUT_DELAY_MS = 80;
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+    
+    public static void Main()
+    {
+        Keyboard._hookIDKey = Keyboard.SetHook(Keyboard._procKey);
+        Mouse._hookIDMouse = Mouse.SetHook(Mouse._procMouse);
+        Application.Run();
+        UnhookWindowsHookEx(Keyboard._hookIDKey);
+        UnhookWindowsHookEx(Mouse._hookIDMouse);
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# IoHooks
+
+This small script attaches itself to the mouse and keyboard input and prints any input actions to the console.
+It serves as a very basic example of how to attach a hook to the mouse and keyboard and how to emulate mouse and keyboard input.
